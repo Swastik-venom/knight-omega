fae88b0e678e4edec76f886ff7a777e87c36d1de
Swastik-venom <github@mail.dangminhhoa.edu.vn>
2025-11-13 17:56:08 +0530

    fixed errors

--- relay/helper/model_mapped.go
+++ relay/helper/model_mapped.go
@@ -7,24 +7,30 @@ import (
 
 	"github.com/QuantumNous/new-api/common"
 	"github.com/QuantumNous/new-api/dto"
-	"github.com/QuantumNous/new-api/relay/common"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func ModelMappedHelper(c *gin.Context, info *common.RelayInfo, request dto.Request) error {
+	logger := common.GetLogger()
+	fields := []zap.Field{
+		zap.String("original_model", info.OriginModelName),
+		zap.String("endpoint", c.Request.URL.Path),
+	}
+
 	// map model name
 	modelMapping := c.GetString("model_mapping")
-	common.SysLog(fmt.Sprintf("Model mapping config: %s", modelMapping))
-	common.SysLog(fmt.Sprintf("Original model: %s", info.OriginModelName))
+	fields = append(fields, zap.String("model_mapping", modelMapping))
 	
 	if modelMapping != "" && modelMapping != "{}" {
 		modelMap := make(map[string]string)
 		err := json.Unmarshal([]byte(modelMapping), &modelMap)
 		if err != nil {
-			common.SysLog(fmt.Sprintf("Failed to unmarshal model mapping: %v", err))
+			logger.Error("Failed to unmarshal model mapping",
+				append(fields, zap.Error(err))...)
 			return fmt.Errorf("unmarshal_model_mapping_failed")
 		}
-		common.SysLog(fmt.Sprintf("Model map: %+v", modelMap))
+		fields = append(fields, zap.Any("model_map", modelMap))
 
 		// 支持链式模型重定向，最终使用链尾的模型
 		currentModel := info.OriginModelName
@@ -55,18 +61,25 @@ func ModelMappedHelper(c *gin.Context, info *common.RelayInfo, request dto.Reque
 		}
 		if info.IsModelMapped {
 			info.UpstreamModelName = currentModel
-			common.SysLog(fmt.Sprintf("Model mapped to: %s", info.UpstreamModelName))
+			fields = append(fields,
+				zap.String("mapped_model", info.UpstreamModelName),
+				zap.Bool("is_mapped", true))
+			logger.Info("Model mapping applied", fields...)
 		}
 	} else {
 		// If no model mapping, use the original model name
 		info.UpstreamModelName = info.OriginModelName
-		common.SysLog(fmt.Sprintf("No model mapping, using original model: %s", info.UpstreamModelName))
+		fields = append(fields,
+			zap.String("final_model", info.UpstreamModelName),
+			zap.Bool("is_mapped", false))
+		logger.Debug("No model mapping applied", fields...)
 	}
 	
 	// Always set the model name in the request
 	if request != nil {
 		request.SetModelName(info.UpstreamModelName)
-		common.SysLog(fmt.Sprintf("Request model set to: %s", info.UpstreamModelName))
+		logger.Debug("Request model set",
+			append(fields, zap.String("request_model", info.UpstreamModelName))...)
 	}
 	return nil
 }
